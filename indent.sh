6f3fc4b3b6b0b03f7a6eee1b45df974cafe9a1cb
Sjors van Gelderen <sjors@openmailbox.org>
2018-11-27 08:49:36 +0100

    Add handy script for calling GNU indent on output files

---
+++ indent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in *.h *.cpp; do
+    [ -f "$i" ] || break
+    gindent "$i" -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1
+    echo "Finished indenting $i"
+done
